7760f5bbfff4eff8f1db1405c894df8c84bb1f0f
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-03-26 06:30:25 +0300

    add search filter

--- product_operation/api/views/office_api_view.py
+++ product_operation/api/views/office_api_view.py
@@ -1,12 +1,15 @@
-from rest_framework.generics import ListAPIView
 from product_operation.api.serializers import OfficeSerializer
-from product_operation.models import Office
 from product_operation.api.pegination import OfficePagination
+from product_operation.models import Office
+from rest_framework import filters
+from rest_framework.generics import ListAPIView
 
 
 class OfficeListView(ListAPIView):
     serializer_class = OfficeSerializer
     queryset = Office.objects.all()
     pagination_class = OfficePagination
-    # def get_queryset(self):
-    #     return super().get_queryset()
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name']
+    ordering_fields = ['name']
+
